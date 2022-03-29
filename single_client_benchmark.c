49f93c7cab203a8151fd17af6230892c8a806a77
Sam Kottler <samkottler@users.noreply.github.com>
2022-03-29 09:28:18 -0500

    added simple single-client benchmark

---
+++ single_client_benchmark.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+#include "ebs.h"
+#include "timing.h"
+
+void usage(char *name) {
+  printf("Usage: %s <server1> <port1> <server2> <port2> [iterations]\n", name);
+  printf("  server1     ip of first server\n");
+  printf("  server2     ip of second server\n");
+  printf("  port1       port of first server\n");
+  printf("  port2       port of second server\n");
+  printf("  iterations  number of iterations to perform (default: 1000)\n");
+}
+
+int main(int argc, char** argv) {
+  if (argc != 5 && argc != 6) {
+    usage(argv[0]);
+    return 1;
+  }
+
+  if (ebs_init(argv[1], argv[2], argv[3], argv[4]) != EBS_SUCCESS) {
+    return 1;
+  }
+
+  int iterations = 1000;
+  if (argc == 6) {
+    iterations = atoi(argv[5]);
+  }
+
+  char buf[4096];
+  int offset = 0;
+  int read_latency = 0;
+  DO_TRIALS(
+    {offset = (offset + 4096) % (1024*1024);},
+    {
+      if (ebs_read(buf, offset) != EBS_SUCCESS) {
+        return 1;
+      }
+    },
+    iterations,
+    read_latency
+  );
+
+  int write_latency = 0;
+  DO_TRIALS(
+    {offset = (offset + 4096) % (1024*1024);},
+    {
+      if (ebs_read(buf, offset) != EBS_SUCCESS) {
+        return 1;
+      }
+    },
+    iterations,
+    write_latency
+  );
+
+  print_result("Read latency", read_latency);
+  print_result("Write latency", write_latency);
+
+  return 0;
+}
---
+++ timing.h
@@ -0,0 +1,51 @@
+#ifndef __TIMING_H
+#define __TIMING_H
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#define NANOS_PER_SEC (1000*1000*1000l)
+
+#define DO_TRIALS(setup, task, trials, var) {                                  \
+  var = NANOS_PER_SEC;                                                         \
+  struct timespec _start, _end;                                                \
+  unsigned long _total_time = 0;                                               \
+  for (int _i = 0; _i < trials; ++_i) {                                        \
+    setup                                                                      \
+    clock_gettime(CLOCK_MONOTONIC, &_start);                                   \
+    task                                                                       \
+    clock_gettime(CLOCK_MONOTONIC, &_end);                                     \
+    unsigned long _nanos = nanos_diff(&_start, &_end);                         \
+    _total_time += _nanos;                                                     \
+    if (_nanos < var) {                                                        \
+      var = _nanos;                                                            \
+    }                                                                          \
+  }                                                                            \
+}
+
+static unsigned long nanos_diff(struct timespec* start, struct timespec* end) {
+  unsigned long nanos = end->tv_nsec - start->tv_nsec;
+  unsigned long secs = end->tv_sec - start->tv_sec;
+  if (end->tv_nsec < start->tv_nsec) {
+    nanos += NANOS_PER_SEC;
+    secs -= 1;
+  }
+  return nanos + NANOS_PER_SEC * secs;
+}
+
+void print_result(char* name, double nanos) {
+  if (nanos == INFINITY) {
+    printf("%-35s %13s\n", name, "N/A");
+    return;
+  }
+  printf("%-35s %10.2f ns", name, nanos);
+  if (nanos > 1000) {
+    printf("%10.2f us", nanos/1000);
+    if (nanos > 1000000) {
+      printf("%10.2f ms", nanos/1000000);
+    }
+  }
+  printf("\n");
+}
+#endif //__TIMING_H
+
