d7efe10b7d3261adac689e3ee10c07f4c0ede9bf
Sam Kottler <samkottler@users.noreply.github.com>
2022-03-31 09:23:30 -0500

    made simple_test check that same data is read that was written

--- simple_test.c
+++ simple_test.c
@@ -4,8 +4,20 @@
 
 int main() {
   ebs_init("localhost", "18001", "localhost", "18002");
-  char buf[4096];
-  printf("%d\n", ebs_write(buf, 0));
-  printf("%d\n", ebs_read(buf, 0));
+  char write_buf[4096];
+  for (int i = 0; i < 4096; ++i) {
+    write_buf[i] = i%256;
+  }
+  char read_buf[4096];
+  printf("%d\n", ebs_write(write_buf, 0));
+  printf("%d\n", ebs_read(read_buf, 0));
+
+  for (int i = 0; i < 4096; ++i) {
+    if (read_buf[i] != write_buf[i]) {
+      printf("Read did not return same data as write\n");
+      return 0;
+    }
+  }
+  printf("Test passed\n");
   return 0;
 }
